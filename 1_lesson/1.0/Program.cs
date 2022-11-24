2bdad76cffaa4357d845d5466ab3855ef58681e0
Kirill <ad777miral@gmail.com>
2022-11-24 18:03:58 +0300

    Task 1.0

--- 1_lesson/1.0/Program.cs
+++ 1_lesson/1.0/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Write a nunber: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num*num);
