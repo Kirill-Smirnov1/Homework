14a0e159c1ad6232c472f30f799ae7fd8030a442
Kirill <ad777miral@gmail.com>
2022-11-24 19:42:44 +0300

    Числа от - до +

---
+++ 1_lesson/4.0/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+int n = int.Parse(a);
+int negn = -n;
+
+while(negn <= n)
+{
+    Console.Write($"{negn}");
+    negn +=1;
+}
