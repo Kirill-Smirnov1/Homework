ab6429bfeb3966b667fcdca2271f20ff1462f6a6
Kirill <ad777miral@gmail.com>
2022-11-24 18:43:21 +0300

    Проверка 2 чисел на квадрат 2го от 1го

---
+++ 1_lesson/2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Write first number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Write second number: ");
+int num1 = int.Parse(Console.ReadLine());
+if(num1 == num*num )
+{
+   Console.WriteLine(" Yes "); 
+}
+
+else
+
+Console.WriteLine(" No ");
