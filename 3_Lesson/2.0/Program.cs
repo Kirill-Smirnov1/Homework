240f18f3e927fa50b4a338d97a6a3a985f4b93f2
Kirill <ad777miral@gmail.com>
2022-11-28 20:45:30 +0300

    Координаты точек

---
+++ 3_Lesson/2.0/Program.cs
@@ -0,0 +1,16 @@
+﻿void Quarters(string a)
+{
+    if (a == "I")
+        Console.WriteLine("x > 0 and y > 0");
+    else if (a == "II")
+        Console.WriteLine("x < 0 and y > 0");
+    else if (a == "III")
+        Console.WriteLine("x < 0 and y < 0");   
+    else if (a == "IV")
+        Console.WriteLine("x > 0 and y <  0");
+    else 
+        Console.WriteLine("error");       
+
+}
+Quarters(Console.ReadLine());
+
