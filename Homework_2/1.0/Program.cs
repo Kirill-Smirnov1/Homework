18a2c6aa4b91603221b165c5123dd77308fb2afd
Kirill <ad777miral@gmail.com>
2022-11-30 15:20:53 +0300

    Задача №1

--- Homework_2/1.0/Program.cs
+++ Homework_2/1.0/Program.cs
@@ -4,7 +4,25 @@
 //456 -> 5
 //782 -> 8
 //918 -> 1
-Console.Write("Введите трехзначное число а: ");
+
+// Вариант мой.
+Console.Write("Введите трехзначное число: ");
 int a = int.Parse(Console.ReadLine());
-int d = a % 100; int c = d / 10;
-Console.Write($" Вторая цифра числа: {c}" );
+if ((-1000 < a && a < -100) || (a < 1000 && a > 100))
+{
+    int d = a % 100; int c = d / 10;
+    Console.Write($"Результат обработки: {Math.Abs(c)}");
+
+}
+else
+Console.Write("Число не трехзначное!");
+// Вариант из семинара (самостоятельно разобран, при минусовых значениях выдает результат с минусом).
+
+//string aa(int bb)
+//{
+//    if((-1000 < bb && bb < -100) || (bb < 1000 && bb > 100))
+//       return $"{bb / 10 % 10}";
+//    return "Число не трехзначное";    
+//}
+//Console.Write(aa(int.Parse(Console.ReadLine())));
+
