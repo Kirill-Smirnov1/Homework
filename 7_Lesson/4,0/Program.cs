78ed67aac9895a72df7c2156c9484fe8e360a01c
Kirill <ad777miral@gmail.com>
2022-12-12 20:56:46 +0300

    Пример 3

---
+++ 7_Lesson/4,0/Program.cs
@@ -0,0 +1,54 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+int[,] FillArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            arr[i, j] = i+j;
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            Console.Write($"{arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+}
+
+int Summ(int[,] arr)
+{
+    int sum = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            if (i == j)
+            {
+                sum += arr[i, j];
+            }
+                
+    } 
+
+    return sum;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(Summ(arr_1));
+
+
+
